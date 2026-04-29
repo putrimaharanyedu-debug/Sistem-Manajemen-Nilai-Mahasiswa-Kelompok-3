bdacaba39fef6aaab8b7cf6f4aeddc037ea3d906
Putri Maharany <putrimaharanyedu@gmail.com>
2026-04-29 21:33:17 +0700

    Upload update dan dokumentasi

--- nilaiMahasiswa.py
+++ nilaiMahasiswa.py
@@ -17,7 +17,6 @@ class Mahasiswa:
         ):
             self.__nilai_tugas = tugas
             self.__nilai_uts = uts
-            self.__nilai_uts = uts
             self.__nilai_uas = uas
             self.hitung_nilai_akhir()
         else:
@@ -63,6 +62,7 @@ class Mahasiswa:
         self.hitung_nilai_akhir()
         print("Nilai berhasil diupdate")
 
+
     def is_lulus(self):
         if self.__nilai_akhir >= 60:
             return "LULUS"
@@ -85,77 +85,3 @@ class Mahasiswa:
         print("Grade :", self.get_grade())
         print("Status:", self.is_lulus())
         print("-" * 30)
-
-
-def main():
-    daftar_mahasiswa = []
-
-    while True:
-        print("\nSISTEM MANAJEMEN NILAI")
-        print("1. Tambah Mahasiswa")
-        print("2. Tampilkan Data")
-        print("3. Update Nilai")
-        print("4. Urutkan Nilai Tertinggi")
-        print("5. Keluar")
-
-        pilihan = input("Pilih menu: ")
-
-        if pilihan == "1":
-            nama = input("Nama : ")
-            nim = input("NIM  : ")
-
-            mhs = Mahasiswa(nama, nim)
-
-            try:
-                tugas = float(input("Nilai Tugas : "))
-                uts = float(input("Nilai UTS   : "))
-                uas = float(input("Nilai UAS   : "))
-
-                mhs.set_nilai(tugas, uts, uas)
-                daftar_mahasiswa.append(mhs)
-
-            except ValueError:
-                print("Input harus angka")
-
-        elif pilihan == "2":
-            for mhs in daftar_mahasiswa:
-                mhs.info()
-
-        elif pilihan == "3":
-            nim = input("Masukkan NIM: ")
-
-            target = next(
-                (
-                    m for m in daftar_mahasiswa
-                    if m.get_nim() == nim
-                ),
-                None
-            )
-
-            if target:
-                jenis = input("Jenis nilai: ")
-                nilai = float(input("Nilai baru : "))
-                target.update_nilai(jenis, nilai)
-            else:
-                print("Mahasiswa tidak ditemukan")
-
-        elif pilihan == "4":
-            daftar_mahasiswa.sort(
-                key=lambda x: x.get_nilai_akhir(),
-                reverse=True
-            )
-
-            print("Data berhasil diurutkan")
-
-            for mhs in daftar_mahasiswa:
-                mhs.info()
-
-        elif pilihan == "5":
-            break
-
-        else:
-            print("Pilihan tidak tersedia")
-
-
-if __name__ == "__main__":
-    main()
